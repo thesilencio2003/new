bed405c31e0bd2340dd426f6a814d82d8cb17610
ale <pruebasdeapp795@gmail.com>
2025-05-14 16:00:57 -0500

    register

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -8,17 +8,17 @@ type AuthStatus = 'checking' | 'authenticated' | 'not-authenticated';
 })
 export class AuthService extends BaseHttpService {
 
- private _authStatus = signal<AuthStatus>('checking');
-private _user = signal<any>(null);
-private _token = signal<String | null>(localStorage.getItem('token'));
+  private _authStatus = signal<AuthStatus>('checking');
+  private _user = signal<any>(null);
+  private _token = signal<String | null>(localStorage.getItem('token'));
 
-authStatus = computed(() => {
-  if (this._authStatus() === 'checking') return 'checking';
-  if (this._user()) return 'authenticated';
-  return 'not-authenticated';
-});
+  authStatus = computed(() => {
+    if (this._authStatus() === 'checking') return 'checking';
+    if (this._user()) return 'authenticated';
+    return 'not-authenticated';
+  });
 
-user = computed(() => this._user());
+  user = computed(() => this._user());
   token = computed(() => this._token());
   isAdmin = computed(() => this._user()?.Role?.name?.includes('admin') ?? false);
 
@@ -26,23 +26,55 @@ user = computed(() => this._user());
     return this.http
       .post<any>(`${this.apiUrl}/auth/login`, { email, password })
       .pipe(
-        map((resp) => {
-          this._user.set(resp.data);
-          this._token.set(resp.data.token);
-          this._authStatus.set('authenticated');
-          localStorage.setItem('token', resp.data.token);
-          localStorage.setItem('user', JSON.stringify(resp.data));
-          return true;
-        }),
-        catchError((error: any) => {
-          this._user.set(null);
-          this._token.set('');
-          this._authStatus.set('not-authenticated');
-          localStorage.removeItem('token');
-          localStorage.removeItem('user');
-          return of(false);
-        })
+        map((resp) => this.handleAuthSuccess(resp)),
+        catchError((error: any) => this.handleAuthError(error))
       );
   }
+
+  register(data: any): Observable<boolean> {
+    return this.http.post<any>(`${this.apiUrl}/auth/register`, data).pipe(
+      map((resp) => this.handleAuthSuccess(resp)),
+      catchError((error: any) => this.handleAuthError(error))
+    );
+  }
+
+  checkStatus(): Observable<boolean> {
+    const token = localStorage.getItem('token');
+    if (!token) {
+      this.logout();
+      return of(false);
+    }
+    return this.http.get<any>(`${this.apiUrl}/auth/check-status`).pipe(
+      map((resp) => this.handleAuthSuccess(resp)),
+      catchError((error: any) => this.handleAuthError(error))
+    );
+  }
+
+
+  logout(): void {
+    this._user.set('');
+    this._token.set('');
+    this._authStatus.set('not-authenticated');
+    localStorage.removeItem('token');
+    localStorage.removeItem('user');
+  }
+
+  private handleAuthSuccess(resp: any) {
+    this._user.set(resp.data.user);
+    this._token.set(resp.data.token);
+    this._authStatus.set('authenticated');
+    localStorage.setItem('token', resp.data.token);
+    localStorage.setItem('user', JSON.stringify(resp.data.user));
+    return true;
+  }
+
+  private handleAuthError(error: any) {
+    this.logout();
+    return of(false);
+  }
+
+
+
+
 
 }
